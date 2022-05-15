770e366eea71b9a0d50dec009bdd3d78995cbe4c
KaterinaTyureva <katerinastyureva@gmail.com>
2022-05-15 17:41:20 +0500

    task 2 not done

--- task_2/solution.js
+++ task_2/solution.js
@@ -4,6 +4,14 @@ function calcShipping(sum, min, shipping) {
     let shippingPrice = shipping; // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
+    let shippingSum;
+    freeShippingMinSum = 700;
+    shippingPrice = 200;
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    } else {
+        shippingSum = shippingPrice;
+    }
 
     // создайте переменную shippingSum
 
@@ -27,7 +35,14 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
-
+    let discountSum;
+    discountMinSum = 700;
+    discountPart = 10; 
+    if (productsSum >= discountMinSum) {
+       discountSum = (discountPart * productsSum) / 100;
+    } else {
+        discountSum = 0;
+    }
     // создайте переменную discountSum
 
     // если productsSum больше или равно discountMinSum,
@@ -44,6 +59,8 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
+    let totalSum = productsSum;
+    totalSum = (totalSum - discountSum);
 
     // создайте переменную totalSum
 
@@ -51,7 +68,9 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
-
+    totalSum = (totalSum + shippingSum);
+    let freeShipping;
+    freeShipping = !(shippingSum)
     // прибавьте к totalSum значение shippingSum
 
     // создайте переменную freeShipping
