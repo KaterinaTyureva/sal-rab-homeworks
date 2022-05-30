514dc2c58e8e0ce0f62f35bc73a5104191256b77
KaterinaTyureva <katerinastyureva@gmail.com>
2022-05-30 23:22:35 +0500

    task final NOT READY

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,11 +34,11 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: {name, phone}, order: {address, sum}, goods: [{title, count}]};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push(goods[i].title);
     }
 
@@ -46,8 +46,13 @@ function sendRequest(name, phone, address, goods, sum) {
     data.order.sum = name + phone + address + goods + sum;
 
     data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    data.phone = '+7(987)65-43-210';
+    data.street = 'ул. Ленина,';
+    data.house = 'дом 2,';
+    data.entrance = '4 подъезд,'; 
+    data.floor = '5 этаж,';
+    data.flat = 'кв 53';
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
